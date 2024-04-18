7fdf40261d814add5664d6894f5760835e6b90e3
sagi <sagi21805@gmail.com>
2024-04-18 23:39:14 +0300

    added the theory now need to implement the function

--- README.txt
+++
@@ -1,3 +0,0 @@
-# Theory
-
-![Theory](https://github.com/sagi21805/Stereo-Vision/blob/main/data/Stereo-calc.jpg)
--- source/include/stereo.hpp
+++ source/include/stereo.hpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+using cv::Mat;
+typedef float float32;
+typedef double float64;
+
+float32 getDepth(float32 cameraAngle, float32 baseLine, float32 angleFromCam1, float32 angleFromCam2) {
+
+    
+
+}
