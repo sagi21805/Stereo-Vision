d4f1dc7847a96f97281eb732fa2579767ae2059d
sagi <sagi21805@gmail.com>
2024-04-22 01:09:01 +0300

    Added a window error calculating function

--- source/src/Stereo.cpp
+++ source/src/Stereo.cpp
@@ -1,4 +1,4 @@
-#include "stereo.hpp"
+#include "Stereo.hpp"
 
 float32 getDepth(Camera cam1, Camera cam2, Point point1, Point point2, float32 baseLine) {
 
@@ -13,13 +13,41 @@ float32 getDepth(Camera cam1, Camera cam2, Point point1, Point point2, float32 b
     
 }
 
+uint32_t windowMSE(Mat img1, Mat img2, uint32_t windowSize, uint32_t imgRow, uint32_t imgCol) {        
 
-// int main() {
-
-//     uint16_t x = 3;
-//     float32 a = 3.25;
-
-//     float32 b = a / x;
-
-//     std::cout << "b: " << b << "\n";
+    uchar* img1Data = img1.ptr();
+    uchar* img2Data = img2.ptr();
+    uint32_t MSE = 0;
+    for (uint32_t windowRow = 0; windowRow < windowSize; windowRow++){
+        for (uint32_t windowCol = 0; windowCol < windowSize; windowCol++){
+            int16_t a = *(img1Data + windowCol + windowRow*img1.cols + imgCol*windowSize + imgRow*img1.cols*windowSize);
+            int16_t b = *(img2Data + windowCol + windowRow*img2.cols + imgCol*windowSize + imgRow*img2.cols*windowSize);
+            MSE += (a-b)*(a-b);
+            
+        }
+    }
+    return MSE;
+}
+//     }
 // }
+
+int main() {
+    uchar testArray[] {
+     0,  1,  2,  3,  4,  5,  6,  7,  8,
+     10, 11, 12, 13, 14, 15, 16, 17, 18,
+     20, 21, 22, 23, 24, 25, 26, 27, 28,
+    };
+
+    uchar testArray2[] {
+     1,  2,  3,  4,  8,  6,  7,  8, 9,
+     11, 12, 13, 14, 15, 16, 17, 18, 19,
+     21, 22, 23, 24, 25, 26, 27, 28, 29
+    };
+
+    cv::Mat A(3, 9, CV_8U);
+    cv::Mat B(3, 9, CV_8U);
+    std::memcpy(A.data, testArray, 3*9*sizeof(uchar));
+    std::memcpy(B.data, testArray2, 3*9*sizeof(uchar));
+
+    std::cout << windowMSE(A, B, 3, 0, 1) << "\n";
+} 
