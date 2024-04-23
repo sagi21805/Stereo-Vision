364baa9bf6a390bbd0d73aa09ee0431bedf78176
sagi <sagi21805@gmail.com>
2024-04-23 12:43:09 +0300

    tested functions in min

--- source/src/Main.cpp
+++ source/src/Main.cpp
@@ -3,7 +3,15 @@
 
 int main() {
 
-    Camera cam(0, Size(2592, 1944), 2571.22);
-    std::cout << cam.getFov() << "\n";
+    Camera cam(0, Size(2864, 1924), 2945.377);
+    Camera cam1(1, Size(2864, 1924), 2945.377);
+
+    Mat img1; cam.getFrame(img1);
+    Mat img2; cam.getFrame(img2);
+
+    uint8_t windowSize = 3;
+    uint32_t totalNumOfWindows = (img1.cols / windowSize);
+
+
 
 }
