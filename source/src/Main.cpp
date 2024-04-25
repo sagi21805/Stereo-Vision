5c7824617f31ebeccf6fe9c49fff36970f7e7067
sagi <sagi21805@gmail.com>
2024-04-25 14:08:42 +0300

    tested the print function

--- source/src/Main.cpp
+++ source/src/Main.cpp
@@ -1,5 +1,25 @@
 #include "Stereo.hpp"
 
+void test() {
+
+    uchar testArray[] {
+     0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+     20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+     30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
+     40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 
+     50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
+     60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 
+     70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 
+     80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 
+     90, 91, 92, 93, 94, 95, 96, 97, 98, 99
+    };
+
+    cv::Mat A(10,10,CV_8U);
+    std::memcpy(A.data, testArray, 10*10*sizeof(uchar));
+    printWindow(A, 3, 0, 1);
+
+}
 
 int main() {
 
@@ -10,8 +30,22 @@ int main() {
     Mat img2; cam.getFrame(img2);
 
     uint8_t windowSize = 3;
-    uint32_t totalNumOfWindows = (img1.cols / windowSize);
+    uint32_t windowsPerCol = (img1.cols / windowSize);
+    uint32_t windowsPerRow = (img1.rows / windowSize);
 
+    std::vector<bool> v(windowsPerRow*windowsPerCol, false); 
+    
+    for (uint32_t imgRow = 0; imgRow < img1.rows; imgRow++) {
+
+        for (uint32_t currentCol = 0; currentCol < windowsPerRow; currentCol++) {
+
+            for (uint32_t matchedCol = 0; matchedCol < windowsPerCol; matchedCol++) {
+
+
+            }
+        }
+
+    }
 
 
 }
