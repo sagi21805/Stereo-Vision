e54b7448a9dfd8af38652511f17a57ec33e83f56
sagi <sagi21805@gmail.com>
2024-04-30 21:55:25 +0300

    added read frame method

--- source/python/_utils.py
+++ source/python/_utils.py
@@ -4,8 +4,7 @@ import numpy as np
 def get_camera(index: int) -> cv2.VideoCapture:
     return cv2.VideoCapture(index)
 
-def get_frame():
-    x = cv2.VideoCapture(0)
-    x.read()
+def read_image(path: str):
+    return cv2.imread(path)
 
     
