f66a8a92ca9fff25e81fd9da9116eab48610efc2
sagi <sagi21805@gmail.com>
2024-05-10 18:25:15 +0300

    Transfered the camera class into python - the camera class in mojo will be a wrapper of this mone

---
+++ source/python/camera.py
@@ -0,0 +1,78 @@
+import numpy as np
+import cv2 
+from time import sleep
+
+class Camera:
+    
+    def __init__(self, 
+                index: int,
+                auto_exposure: bool = True,
+                exposure: int = 157,
+                brightness: int = 0,
+                contrast: int = 32,
+                saturation: int = 90,
+                gain: int = 0,
+                frame_width: int = 1280,
+                frame_height: int = 720):
+        
+        self.cap = Camera.initialize_cap(
+                      index, 
+                      auto_exposure, 
+                      exposure, 
+                      brightness, 
+                      contrast, 
+                      saturation, 
+                      gain, 
+                      frame_width, 
+                      frame_height
+                    )
+        
+        self.index = index
+        self.warm()
+        
+
+    def update_frame(self):
+        success, frame = self.cap.read()
+        self.frame = frame if success else np.empty(0)
+        
+    @staticmethod
+    def initialize_cap(
+                index: int,
+                auto_exposure: bool = True,
+                exposure: int = 157,
+                brightness: int = 0,
+                contrast: int = 32,
+                saturation: int = 90,
+                gain: int = 0,
+                frame_width: int = 1280,
+                frame_height: int = 720) -> None:
+        
+        cap = cv2.VideoCapture(index)
+        print(frame_width)
+        print(frame_height)
+        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*"MJPG"))
+        cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3 - (not auto_exposure).__int__()*2)
+        cap.set(cv2.CAP_PROP_EXPOSURE, exposure)
+        cap.set(cv2.CAP_PROP_BRIGHTNESS, brightness)
+        cap.set(cv2.CAP_PROP_CONTRAST, contrast)
+        cap.set(cv2.CAP_PROP_SATURATION, saturation)
+        cap.set(cv2.CAP_PROP_GAIN, gain)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+        
+        return cap
+        
+    def window_frame(self, window_size):
+        
+        self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        
+        self.windowed_frame = np.lib.stride_tricks \
+              .sliding_window_view(self.frame, (window_size, window_size)) \
+              [::window_size, ::window_size].copy()
+              
+    def warm(self):
+        for _ in range(10):
+            self.update_frame()
+              
+    def write_frame(self):
+        cv2.imwrite(f"camera{self.index}.png", self.frame)
