728da68ec624969063bcd52dc60b74546d5b575d
sagi <sagi21805@gmail.com>
2024-05-12 22:18:51 +0300

    Wrote the utils in a better way

--- source/python/_utils.py
+++ source/python/_utils.py
@@ -2,8 +2,19 @@ import cv2
 import numpy as np
 import ctypes
 
-def write_ptr(ptr: int, img_rows: int, img_cols: int):
-    data_pointer = ctypes.cast(ptr, ctypes.POINTER(ctypes.c_float))
-    arr = np.ctypeslib.as_array(data_pointer, shape = (img_rows, img_cols))
-    cv2.imwrite("ptr.png", arr)
+DATA_TYPES = [
+    ctypes.c_uint8,
+    ctypes.c_float
+]
+
+
+def ptr_to_numpy(ptr: int, data_type: int, shape: tuple):
+    global DATA_TYPES
+    data_pointer = ctypes.cast(ptr, ctypes.POINTER(DATA_TYPES[data_type]))
+    return np.ctypeslib.as_array(data_pointer, shape = shape)
+
+def write_ptr(ptr: int, data_type: int, shape: tuple):
+    cv2.imwrite("ptr.png", ptr_to_numpy(ptr, data_type, shape))
     
+def repeat_elements(array, times):
+    return np.repeat(array, times)
