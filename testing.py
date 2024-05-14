4aabdf69f472135ce603162bbe364b34441d1475
sagi <sagi21805@gmail.com>
2024-05-14 23:40:27 +0300

    added a power of 2 rounding functions

--- testing.py
+++ testing.py
@@ -1,8 +1,9 @@
 import time
 import cv2
-
+import math
 import numpy as np
 from numba import prange, njit
+np.set_printoptions(threshold=np.inf)
 
 def repeat_elements(array, times):
     repeated = np.repeat(array, times)
@@ -13,7 +14,13 @@ def repeat_elements(array, times):
 
 
 arr = np.array([[[1,   2,  3,  4], [5,   6,  7,  8], [9,  10, 11, 12], [13, 14, 15, 16], [1,   2,  3,  4], [5,   6,  7,  8]], 
-                [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32], [17, 18, 19, 20], [21, 22, 23, 24]]], dtype = np.uint8)
+                [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32], [17, 18, 19, 20], [21, 22, 23, 99]],
+                [[1,   2,  3,  4], [5,   6,  7,  8], [9,  10, 11, 12], [13, 14, 15, 16], [1,   2,  3,  4], [5,   6,  7,  8]],
+                [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32], [17, 18, 19, 20], [21, 22, 23, 100]],
+                [[1,   2,  3,  4], [5,   6,  7,  8], [9,  10, 11, 12], [13, 14, 15, 16], [1,   2,  3,  4], [5,   6,  7,  8]],
+                [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32], [17, 18, 19, 20], [21, 22, 23, 101]],
+                [[1,   2,  3,  4], [5,   6,  7,  8], [9,  10, 11, 12], [13, 14, 15, 16], [1,   2,  3,  4], [5,   6,  7,  8]],
+                [[17, 18, 19, 20], [21, 22, 23, 24], [25, 26, 27, 28], [29, 30, 31, 32], [17, 18, 19, 20], [21, 22, 23, 101]]], dtype = np.uint8)
 
 image = cv2.imread("data/im1-min.jpeg")
 
@@ -22,17 +29,36 @@ def prepare_arr(image: np.ndarray):
     image = cv2.resize(image, (1280, 720))
     return image
 
-@njit(fastmath = True)
-def window_interleave(image: np.ndarray, window_size):
-    windowed_arr = np.lib.stride_tricks \
+@njit(fastmath = True, parallel = True)
+def window_interleave(image: np.ndarray, window_size: int):
+    windowed_arr: np.ndarray = np.lib.stride_tricks \
                 .sliding_window_view(image, (window_size, window_size, 4)) \
-                [::window_size, ::window_size][0].copy()
-    c = np.empty((windowed_arr.size, ), dtype=windowed_arr.dtype)
-    for i in prange(windowed_arr.shape[0]):
-        c[i::windowed_arr.shape[0]] = windowed_arr[i].flatten()
-    return c.copy()
+                [::window_size, ::window_size].copy()
+    windowed_arr = windowed_arr.reshape((windowed_arr.shape[0], windowed_arr.shape[1],window_size,window_size,4))
+    print(windowed_arr.shape)
+
+    size = windowed_arr[0].flatten().shape[0]
+    size2 = int(closet_power_of_2(size)) - size
+    c = np.zeros((windowed_arr.size + (windowed_arr.shape[0] * size2), ), dtype=np.uint8)
+    for j in prange(windowed_arr.shape[0]):
+        for i in prange(windowed_arr.shape[1]):
+            c[i+j*(size+size2):size*(j+1)+j*size2:windowed_arr.shape[1]] = windowed_arr[j][i].flatten()
+        c[i+j*size]
+    
+    return c
+
+@njit(fastmath = True)
+def closet_power_of_2(num: int):
+    return np.power(2, np.ceil(np.log2(num)))
 
 
+print(closet_power_of_2(1280))
 
+# while True:
+
+
+t = time.time()
 a = window_interleave(arr, 2)
 print(a)
+print(time.time() - t)
+print(a[:65])
