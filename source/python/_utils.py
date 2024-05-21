8e55c7c19e071efcc5c777744b3ed1ff7ce739d9
sagi <sagi21805@gmail.com>
2024-05-21 19:46:59 +0300

    changed the sorting to be from per row instead of per image so it can be loaded into a simd

--- source/python/_utils.py
+++ source/python/_utils.py
@@ -3,12 +3,7 @@ import numpy as np
 import ctypes
 from numba import njit, prange
 
-DATA_TYPES = [
-    ctypes.c_uint8,
-    ctypes.c_uint16 ,
-    ctypes.c_uint32, 
-    ctypes.c_float
-    ]
+DATA_TYPES = [ctypes.c_uint8, ctypes.c_uint16, ctypes.c_uint32, ctypes.c_float]
 
 
 def ptr_to_numpy(ptr: int, dtype: int, shape: tuple):
@@ -30,38 +25,32 @@ def repeat_elements(array, times):
 def closet_power_of_2(num: int):
     return int(np.power(2, np.ceil(np.log2(num))))
 
+
 def pad_array(bgra: np.ndarray):
-    pad_offset_cols = (
-        closet_power_of_2(bgra.shape[1]) - bgra.shape[1]
-    )
+    pad_offset_cols = closet_power_of_2(bgra.shape[1]) - bgra.shape[1]
     padded = np.pad(
         bgra,
         ((0, 0), (0, pad_offset_cols), (0, 0)),
         "constant",
         constant_values=255,
     )
-    pad_offset_rows = (
-        closet_power_of_2(bgra.shape[0]) - bgra.shape[0]
-    )
+    pad_offset_rows = closet_power_of_2(bgra.shape[0]) - bgra.shape[0]
     padded = np.pad(
         padded,
         ((0, pad_offset_rows), (0, 0), (0, 0)),
         "constant",
         constant_values=255,
     )
-    
+
     return padded
 
-@njit
-def window_bgra(
-    padded_bgra: np.ndarray, 
-    window_size: int
-    ):
 
+@njit
+def window_bgra(padded_bgra: np.ndarray, window_size: int):
     windowed_frame = np.lib.stride_tricks.sliding_window_view(
         padded_bgra, (window_size, window_size, 4)
     )[::window_size, ::window_size].copy()
-    
+
     windowed_frame = windowed_frame.reshape(
         (
             windowed_frame.shape[0],
@@ -74,68 +63,49 @@ def window_bgra(
 
     return windowed_frame
 
+
 @njit(fastmath=True, parallel=True)
 def sort_windowed_bgrabgra(
     windowed_frame: np.ndarray,
-    window_size: int
-    ):
+    window_size: int,
+    wpr_padded: int,
+    wpc_padded: int,
+    out_arr: np.ndarray
+):
+    # 4 is elements per window (r, g, b, a)
 
-    elements_per_window = 4  # b, g, r, a
-
-    bgrabgra = np.empty(
-        (
-            windowed_frame.size // elements_per_window,
-            elements_per_window,
-        ),
-        dtype=np.uint8,
-    )
-
+    c = np.empty((windowed_frame.size // 4, 4), dtype=np.uint8)
     windowed_frame = windowed_frame.reshape(
         (
             windowed_frame.shape[0] * windowed_frame.shape[1],
             window_size * window_size,
-            elements_per_window,
+            4,
         )
     )
-
-    for window in prange(windowed_frame.shape[0]):
-        bgrabgra[
-            window : bgrabgra.shape[0] : windowed_frame.shape[0]
-        ] = windowed_frame[window]
     
-    return bgrabgra
+    per_row_offset = wpr_padded * window_size * window_size
+    
+    for row in prange(wpc_padded):
+        for col in prange(wpr_padded):
+            out_arr[
+                col + per_row_offset * row : 
+                per_row_offset * (row + 1) : 
+                wpr_padded
+            ] = windowed_frame[col + row * wpr_padded]
+
+    return c
+
 
 @njit(fastmath=True, parallel=True)
 def sort_windowed_bbggrraa(
-    bgrabgra: np.ndarray,
-    window_size: int,
-    windows_per_row: int, 
-    windows_per_col: int
+    windowed_frame: np.ndarray, special_windowed_frame: np.ndarray
 ):
+    size = windowed_frame[0].flatten().shape[0]
+    rounded = closet_power_of_2(size)
 
-    bbggrraa = np.empty(
-        (bgrabgra.size,),
-        dtype=np.uint8,
-    )
-
-    for window_element in prange(window_size * window_size):
-        for row in prange(windows_per_col):
-            for col in prange(windows_per_row):
-                total_row_elements = (
-                    windows_per_row * 4 # 4 -> elements per pixel (b, g, r, a)
-                )
-                bbggrraa[
-                    col
-                    + (row + window_element * windows_per_col)
-                    * total_row_elements : (
-                        (row + 1) + window_element * windows_per_col
-                    )
-                    * total_row_elements : windows_per_row
-                ] = bgrabgra[
-                    col
-                    + (row + window_element * windows_per_col)
-                    * windows_per_row
-                ]
-    
-    return bbggrraa
-
+    for row in prange(windowed_frame.shape[0]):
+        total = row * rounded
+        for col in prange(windowed_frame.shape[1]):
+            special_windowed_frame[
+                col + total : size + total : windowed_frame.shape[1]
+            ] = windowed_frame[row][col].flatten()
