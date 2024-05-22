5534d0c9c3db27db0f577746c2923fab777ab77a
sagi <sagi21805@gmail.com>
2024-05-22 20:14:03 +0300

    Created a cam settings module that will help to create a camera with the proper settings

---
+++ source/python/cam_settings.py
@@ -0,0 +1,41 @@
+import cv2
+
+class CamSettings:
+       
+    def __init__(self, 
+                 frame_width: int = 1280,
+                 frame_height: int = 720,
+                 auto_exposure: bool = True,
+                 exposure: int = 157,
+                 brightness: int = 0,
+                 contrast: int = 32,
+                 saturation: int = 90,
+                 gain: int = 0,
+                 fake: str = ""
+        ) -> None:
+
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.auto_exposure = auto_exposure
+        self.exposure = exposure
+        self.brightness = brightness
+        self.contrast = contrast
+        self.saturation = saturation
+        self.gain = gain
+        self.fake = fake
+
+    def initialize_cap(self, index) -> None:
+        cap = cv2.VideoCapture(index)
+        cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter.fourcc(*"MJPG"))
+        cap.set(
+            cv2.CAP_PROP_AUTO_EXPOSURE, 3 - (not self.auto_exposure).__int__() * 2
+        )
+        cap.set(cv2.CAP_PROP_EXPOSURE, self.exposure)
+        cap.set(cv2.CAP_PROP_BRIGHTNESS, self.brightness)
+        cap.set(cv2.CAP_PROP_CONTRAST, self.contrast)
+        cap.set(cv2.CAP_PROP_SATURATION, self.saturation)
+        cap.set(cv2.CAP_PROP_GAIN, self.gain)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.frame_width)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frame_height)
+
+        return cap
