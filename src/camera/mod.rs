61c2fc66c5c807b14bf53f68fb0fa89b2618301c
sagi <sagi21805@gmail.com>
2024-06-12 16:59:12 +0300

    Added a lifetime for the camera and a frame slice that is just the slice of the current frame

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,20 +1,22 @@
 pub mod cam_utils;
+use std::rc::Rc;
+use super::utils;
 use opencv::prelude::VideoCaptureTrait;
 use opencv::videoio::VideoCapture;
-use opencv::imgcodecs::IMWRITE_PNG_COMPRESSION;
 use cam_utils::{CamParameters, CamSettings};
 use opencv::core::*;
 
-pub struct Camera {
+pub struct Camera<'a> {
 
     index: i32,
     cap: VideoCapture,
     params: CamParameters,
     settings: CamSettings,
-    pub frame: Mat
+    pub frame: Mat,
+    pub frame_slice: Option<Rc<&'a [u8]>>
 }
 
-impl Camera {
+impl<'a> Camera<'a> {
 
     pub fn new(index: i32, settings: CamSettings, params: CamParameters) -> Self {
 
@@ -23,7 +25,8 @@ impl Camera {
             cap: settings.initialize_cap(index).unwrap(),
             params,
             settings,
-            frame: Mat::default()
+            frame: Mat::default(),
+            frame_slice: None
         }
 
     }
@@ -31,14 +34,14 @@ impl Camera {
     pub fn update_frame(&mut self) {
 
         self.cap.read(&mut self.frame).expect("Couldn't read frame");
+        self.frame_slice = utils::mat_to_slice(&self.frame);
 
     }
 
     pub fn write_frames(&self) {
-        let mut params = Vector::new();
-        params.push(IMWRITE_PNG_COMPRESSION);
-        params.push(self.params.compersion_level); // Compression level (0-9, where 9 is the highest)
-        opencv::imgcodecs::imwrite("test.png", &self.frame, &params).unwrap();
+        opencv::imgcodecs::imwrite(
+            "test.png", &self.frame, &self.params.png_params
+        ).unwrap();
     }
 
 
