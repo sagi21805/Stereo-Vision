526b989951bdaf24df962236efece4b4a60217a6
sagi <sagi21805@gmail.com>
2024-06-12 16:59:28 +0300

    updated the mat to slice to return option

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,17 +2,15 @@ use opencv::{imgproc, prelude::*};
 use std::rc::Rc;
 pub mod point;
 
-pub fn mat_to_slice(mat: &Mat) -> Result<Rc<&[u8]>, &str> {
+pub fn mat_to_slice<'a>(mat: &Mat) -> Option<Rc<&'a [u8]>> {
     // Check if the Mat is continuous
-    if !mat.is_continuous() {
-        return Err("Mat isn't continuous, can't turn into a slice");
-    }
+    if !mat.is_continuous() { return None; }
 
     let data_ptr = mat.data() as *const u8;
     let total = (mat.total() * mat.elem_size().unwrap()) as usize;
     println!("total size: {}", total);
 
-    Ok(unsafe { 
+    Some(unsafe { 
         Rc::new(std::slice::from_raw_parts(data_ptr, total)) 
     })
 }
