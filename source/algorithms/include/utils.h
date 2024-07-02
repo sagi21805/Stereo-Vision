24b0e418549b426a029cfd25ac381ca22ac5aa76
sagi <sagi21805@gmail.com>
2024-07-02 14:50:08 +0300

    Added more utils funcs

--- source/algorithms/include/utils.h
+++ source/algorithms/include/utils.h
@@ -1,12 +1,40 @@
 #ifndef UTILS
 #define UTILS
 
-#include "stdio.h"
-#include "stdint.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <time.h>
+#include "simd.h"   
+
+#define MEASURE_TIME(FCALL) ({ double START = clock(); FCALL; double TIME = ((double) clock() - START) / CLOCKS_PER_SEC; printf("time: %f\n", TIME);})
+
+typedef struct {
+    uint8_t* a;
+    size_t size;
+} Slice;
 
 
 
-static inline void print_int_ptr(int* p, int length){
+Slice createSlice(uint8_t *data, size_t size);
+
+uint8_t* spaced_points(uint8_t num_of_points, uint8_t val);
+
+uint8_t* generate_pointer(uint8_t start, uint8_t stop);
+
+uint32_t sum_uint8_arr(uint8_t* arr, size_t size);
+
+static inline void print_int_ptr(const int32_t* p, size_t length){
+
+    printf("[");
+    for (int i = 0; i < length; i++){
+        printf("%d ", p[i]);
+    }
+    printf("\b]\n");
+
+}
+
+static inline void print_uint8_ptr(const uint8_t* p, size_t length){
 
     printf("[");
     for (int i = 0; i < length; i++){
@@ -16,7 +44,7 @@ static inline void print_int_ptr(int* p, int length){
 
 }
 
-static inline void print_uint8_ptr(uint8_t* p, int length){
+static inline void print_uint16_ptr(const uint16_t* p, size_t length){
 
     printf("[");
     for (int i = 0; i < length; i++){
@@ -26,7 +54,7 @@ static inline void print_uint8_ptr(uint8_t* p, int length){
 
 }
 
-static inline void print_uint16_ptr(uint16_t* p, int length){
+static inline void print_uint32_ptr(const uint32_t* p, size_t length){
 
     printf("[");
     for (int i = 0; i < length; i++){
@@ -35,5 +63,6 @@ static inline void print_uint16_ptr(uint16_t* p, int length){
     printf("\b]\n");
 
 }
+
 
 #endif
---
+++ source/algorithms/src/utils.c
@@ -0,0 +1,70 @@
+#include "utils.h"
+
+Slice createSlice(uint8_t *data, size_t size) {
+    Slice slice;
+    slice.a = data;
+    slice.size = size;
+    return slice;
+}
+
+uint8_t* spaced_points(uint8_t num_of_points, uint8_t val){
+    
+    uint8_t* points = (uint8_t*) malloc(num_of_points);
+
+    points[0] = 0;
+    uint8_t step = val / (num_of_points - 1);
+
+    for (int i = 1; i < num_of_points; i++) {
+
+        points[i] = points[i - 1] + step;
+    
+    }
+
+    return points;
+
+}
+
+uint8_t* generate_pointer(uint8_t start, uint8_t stop) {
+
+    uint8_t size = (stop - start);
+
+    uint8_t *ptr = (uint8_t *)malloc(size * sizeof(uint8_t));
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+
+    for (uint8_t i = 0, value = start; i < size; ++i, value++) {
+        ptr[i] = (uint8_t)value;
+    }
+
+    return ptr;
+}
+
+uint32_t sum_uint8_arr(uint8_t* arr, size_t size){
+
+    uint32_t sum = 0;
+    size_t left_size = size/2;
+    size_t right_size = size - left_size;
+
+    while (left_size != 1) {
+
+        uint16_t temp[left_size];
+
+        add_arrays_u8_u8_u16(arr, arr + left_size, temp, left_size);
+
+        if (left_size < right_size) {
+            sum += arr[size - 1];
+        } 
+
+        size_t new_size = left_size;
+
+        left_size = new_size/2;
+        right_size = new_size - left_size;
+
+    }
+
+    
+
+}
