7900a4302b68378978e4be15c28c46613349e88f
sagi <sagi21805@gmail.com>
2024-07-02 14:49:59 +0300

    Added window filters

--- source/algorithms/src/window_filters.c
+++ source/algorithms/src/window_filters.c
@@ -2,33 +2,60 @@
 #include <malloc.h>
 #include <memory.h>
 #include <arm_neon.h>
+#include "utils.h"
 
-
-uint8_t* slidingWindowThresholdOptimized(
-        uint8_t* img, int32_t rows, int32_t cols, int32_t window_size, uint8_t threshold
+uint8_t* slidingWindowThreshold(
+        uint8_t* img, int32_t rows, int32_t cols, uint32_t window_size, uint8_t threshold
     ){
-    
     const uint32_t movementRows = (rows - window_size) + 1;
     const uint32_t movementCols = (cols - window_size) + 1;
     
     const uint32_t thresh = threshold*window_size*window_size;
     uint32_t sum = 0;
-    int32_t offset = 1-window_size; // second
     uint8_t* out = (uint8_t*) malloc(movementCols*movementRows); //sizeof(uin8_t is 1)
     memset(out, 0, movementCols*movementRows);
+    int32_t offset = 1-window_size;
 
     for (uint32_t imgRow = 0; imgRow < movementRows; imgRow++){
-        offset += window_size-1; //second
+        offset += window_size-1;
         for (uint32_t imgCol = 0; imgCol < movementCols; imgCol++){
-
             for (uint32_t windowRow = 0; windowRow < window_size; windowRow++){
                 for (uint32_t windowCol = 0; windowCol < window_size; windowCol++){
-                    sum += *(img + windowCol + windowRow*cols + imgCol + imgRow*cols + offset);
+                    sum += *(img + windowCol + windowRow*cols + imgCol + imgRow*movementCols + offset);
                 }
             }
             if (sum > thresh){
-                *(out + imgCol + imgRow*movementRows) = 255;
+                out[imgCol + imgRow*movementCols] = 255;
             }
+            sum = 0;
+        }
+    }
+    return out;
+}
+
+uint8_t* slidingWindowMultiThreshold(
+        uint8_t* img, int32_t rows, int32_t cols, uint32_t window_size, uint8_t thresholds_num
+    ){
+    const uint32_t movementRows = (rows - window_size) + 1;
+    const uint32_t movementCols = (cols - window_size) + 1;
+    
+    const uint8_t* vals = spaced_points(thresholds_num, 255); // 255 is the max val of an image;
+    const uint32_t devisor = window_size*window_size*255 / thresholds_num;
+
+
+    uint32_t sum = 0;
+    uint8_t* out = (uint8_t*) malloc(movementCols*movementRows); //sizeof(uin8_t is 1)
+    int32_t offset = 1-window_size;
+
+    for (uint32_t imgRow = 0; imgRow < movementRows; imgRow++){
+        offset += window_size-1;
+        for (uint32_t imgCol = 0; imgCol < movementCols; imgCol++){
+            for (uint32_t windowRow = 0; windowRow < window_size; windowRow++){
+                for (uint32_t windowCol = 0; windowCol < window_size; windowCol++){
+                    sum += *(img + windowCol + windowRow*cols + imgCol + imgRow*movementCols + offset);
+                }
+            }
+            out[imgCol + imgRow*movementCols] = vals[sum / devisor];
             sum = 0;
         }
     }
