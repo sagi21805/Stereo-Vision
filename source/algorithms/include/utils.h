1d61397ac223ed55d7cf2976bbc726b9d840c80b
sagi <sagi21805@gmail.com>
2024-07-03 10:48:53 +0300

    MEASURE_TIME now is testing the func call 10 times

--- source/algorithms/include/utils.h
+++ source/algorithms/include/utils.h
@@ -7,7 +7,15 @@
 #include <time.h>
 #include "simd.h"   
 
-#define MEASURE_TIME(FCALL) ({ double START = clock(); FCALL; double TIME = ((double) clock() - START) / CLOCKS_PER_SEC; printf("time: %f\n", TIME);})
+#define MEASURE_TIME(FCALL) ({ \
+    double TOTAL = 0; \
+    int REPEAT = 10; \
+    for (int i = 0; i < REPEAT; i++) { \
+        double START = clock(); \
+        FCALL; \
+        TOTAL += ((double) clock() - START) / CLOCKS_PER_SEC; \
+    } \
+    printf("time: %f\n", TOTAL / REPEAT);})
 
 typedef struct {
     uint8_t* a;
@@ -15,14 +23,18 @@ typedef struct {
 } Slice;
 
 
-
 Slice createSlice(uint8_t *data, size_t size);
 
 uint8_t* spaced_points(uint8_t num_of_points, uint8_t val);
 
-uint8_t* generate_pointer(uint8_t start, uint8_t stop);
+uint8_t* generate_pointer_u8(uint8_t start, uint8_t stop);
+
+uint16_t* generate_pointer_u16(uint16_t start, uint16_t stop);
+
+uint32_t* generate_pointer_u32(uint32_t start, uint32_t stop);
 
 uint32_t sum_uint8_arr(uint8_t* arr, size_t size);
+
 
 static inline void print_int_ptr(const int32_t* p, size_t length){
 
