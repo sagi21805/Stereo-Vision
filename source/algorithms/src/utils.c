2d653b7730204d3b0963e4466314a2e50aade3dc
sagi <sagi21805@gmail.com>
2024-07-03 10:48:22 +0300

    Added more pointer generation functions

--- source/algorithms/src/utils.c
+++ source/algorithms/src/utils.c
@@ -10,7 +10,7 @@ Slice createSlice(uint8_t *data, size_t size) {
 uint8_t* spaced_points(uint8_t num_of_points, uint8_t val){
     
     uint8_t* points = (uint8_t*) malloc(num_of_points);
-
+    
     points[0] = 0;
     uint8_t step = val / (num_of_points - 1);
 
@@ -24,7 +24,7 @@ uint8_t* spaced_points(uint8_t num_of_points, uint8_t val){
 
 }
 
-uint8_t* generate_pointer(uint8_t start, uint8_t stop) {
+uint8_t* generate_pointer_u8(uint8_t start, uint8_t stop) {
 
     uint8_t size = (stop - start);
 
@@ -37,6 +37,42 @@ uint8_t* generate_pointer(uint8_t start, uint8_t stop) {
 
     for (uint8_t i = 0, value = start; i < size; ++i, value++) {
         ptr[i] = (uint8_t)value;
+    }
+
+    return ptr;
+}
+
+uint16_t* generate_pointer_u16(uint16_t start, uint16_t stop) {
+
+    uint16_t size = (stop - start);
+
+    uint16_t *ptr = (uint16_t *)malloc(size * sizeof(uint16_t));
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+
+    for (uint16_t i = 0, value = start; i < size; ++i, value++) {
+        ptr[i] = (uint16_t)value;
+    }
+
+    return ptr;
+}
+
+uint32_t* generate_pointer_u32(uint32_t start, uint32_t stop) {
+
+    uint32_t size = (stop - start);
+
+    uint32_t *ptr = (uint32_t *)malloc(size * sizeof(uint32_t));
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+
+    for (uint32_t i = 0, value = start; i < size; ++i, value++) {
+        ptr[i] = (uint32_t)value;
     }
 
     return ptr;
