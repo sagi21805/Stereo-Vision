0f01538253f3f2c26be321c48abfdc70b3fbae97
sagi <sagi21805@gmail.com>
2024-07-11 11:28:20 +0300

    Added more tests

--- source/algorithms/src/lib.rs
+++ source/algorithms/src/lib.rs
@@ -18,9 +18,7 @@ fn algorithms(py: Python, m: &PyModule) -> PyResult<()> {
 mod tests {
 
     use crate::integral_image::{
-        self, 
-        integral_image_neon::neon_integral_image,
-        integral_image::rectangle_sum
+        self, integral_image::rectangle_sum, integral_image_neon::neon_integral_image,
     };
 
     use ndarray::Array2;
@@ -29,40 +27,54 @@ mod tests {
     fn test_integral_sum() {
         let source_image: Vec<u8> = vec![
             1, 2, 3, 4, 
-            5, 6, 7, 8,
-            9, 10, 11, 12,
+            5, 6, 7, 8, 
+            9, 10, 11, 12, 
             13, 14, 15, 16
         ];
 
         let width = 4;
         let height = 4;
 
-        let integral_image = neon_integral_image(
-            source_image.as_slice(),
-             width, 
-             height
-        );
+        let integral_image = neon_integral_image(source_image.as_slice(), width, height);
 
-        for i in 0..height+1 {
-            for j in 0..width+1 {
-                print!("{} ", integral_image[i * (width+1) + j]);
+        for i in 0..height + 1 {
+            for j in 0..width + 1 {
+                print!("{} ", integral_image[i * (width + 1) + j]);
             }
             println!();
         }
 
-        let x = 0;
-        let y = 0;
-        let rect_width = 3;
+        let x = 1;
+        let y = 1;
+        let rect_width = 2;
         let rect_height = 2;
 
-        let sum = rectangle_sum(
-            &integral_image,
-            width + 1,
-            x, 
-            y, 
-            rect_width,
-            rect_height
-        );
-        assert_eq!(sum, 24);
+        let sum = rectangle_sum(&integral_image, width + 1, x, y, rect_width, rect_height);
+        assert_eq!(sum, 34);
+    }
+
+    #[test]
+    fn black_white() {
+        let source_image: Vec<u8> = vec![0, 255, 0, 0, 0, 0, 255, 255, 0, 255, 0, 0, 0, 0, 0, 255];
+
+        let width = 4;
+        let height = 4;
+
+        let integral_image = neon_integral_image(source_image.as_slice(), width, height);
+
+        for i in 0..height + 1 {
+            for j in 0..width + 1 {
+                print!("{} ", integral_image[i * (width + 1) + j]);
+            }
+            println!();
+        }
+
+        let x = 1;
+        let y = 1;
+        let rect_width = 2;
+        let rect_height = 2;
+
+        let sum = rectangle_sum(&integral_image, width + 1, x, y, rect_width, rect_height);
+        assert_eq!(sum, 510);
     }
 }
