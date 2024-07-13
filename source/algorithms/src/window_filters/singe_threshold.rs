a9807dc8b4a40969cc83de786cea6be2cce26575
sagi <sagi21805@gmail.com>
2024-07-13 13:30:56 +0300

    updated naming

--- source/algorithms/src/window_filters/singe_threshold.rs
+++ source/algorithms/src/window_filters/singe_threshold.rs
@@ -8,17 +8,17 @@ pub fn sliding_window_threshold(
     window_size: usize,
     threshold: u8,
 ) -> Vec<u8> {
-    let movement_rows = rows - window_size + 1;
-    let movement_cols = cols - window_size + 1;
+    let final_rows = rows - window_size + 1;
+    let final_cols = cols - window_size + 1;
 
     let thresh = threshold as usize * window_size * window_size;
-    let mut out = vec![0u8; movement_cols * movement_rows];
+    let mut out = vec![0u8; final_cols * final_rows];
 
     out.par_iter_mut()
         .enumerate()
         .for_each(|(index, out_pixel)| {
-            let img_row = index / movement_cols;
-            let img_col = index % movement_cols;
+            let img_row = index / final_cols;
+            let img_col = index % final_cols;
             let mut sum: u32 = 0;
 
             for window_row in 0..window_size {
