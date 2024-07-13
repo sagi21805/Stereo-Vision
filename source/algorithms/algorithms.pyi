9706c9bffb20c8c89cd2942ec601a029b11fe054
sagi <sagi21805@gmail.com>
2024-07-13 12:57:48 +0300

    updated functions

--- source/algorithms/algorithms.pyi
+++ source/algorithms/algorithms.pyi
@@ -2,8 +2,10 @@ from typing import Any
 import numpy as np
 
 class IntegralImage:
-    integral_image: np.ndarray[Any, np.dtype[np.uint32]]
 
+    integral_image: np.ndarray[Any, np.dtype[np.uint32]]
+    source_image: np.ndarray[Any, np.dtype[np.uint8]]
+    
     def __new__(
             cls, 
             source_image: np.ndarray[Any, np.dtype[np.uint8]]
@@ -14,6 +16,31 @@ class IntegralImage:
             window_size: int, 
             thresholds_num: int
     ) -> np.ndarray[np.uint8]: ...
+
+    def adaptive_window_threshold(
+            self, 
+            window_size: int, 
+            threshold: int,
+    ) -> np.ndarray[Any, np.dtype[np.uint8]]: ...
+
+    def sliding_window_threshold(
+        self,
+        window_size: int,
+        threshold: int
+    ) -> np.ndarray[Any, np.dtype[np.uint8]]: ...
+
+    def threshold_integral(
+        self, 
+        t: float
+    ) -> np.ndarray[Any, np.dtype[np.uint8]]: ...
+
+    def adaptive_window_mean_threshold(
+        self, 
+        window_size: int, 
+        threshold_factor: float
+    ) -> np.ndarray[Any, np.dtype[np.uint8]]: ...
+
+
 
 def sliding_window_multi_threshold(
     img: np.ndarray[Any, np.dtype[np.uint8]], 
